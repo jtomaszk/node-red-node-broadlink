74d21cad1d68b6d782f1b9ce0709c8e29eb35b6b
Jarema Tomaszkiewicz <jarema.tomaszkiewicz@gmail.com>
2017-04-05 17:01:02 +0000

    broadlink configuration node

--- broadlink_in.js
+++ broadlink_in.js
@@ -1,12 +1,33 @@
+var devices = [];
+
+var dns = require('dns');
+
 module.exports = function(RED) {
     function createBroadlink(node, global, broadlink) {
        var b = new broadlink();
        global.set('broadlink', b);
 
        b.on("deviceReady", (dev) => {
+           dns.reverse(dev.host.address, function(err,domains){
+               var name = '';
+               if (err != null) {
+                   node.log(err);
+                   name = dev.host.address;
+               } else {
+                   node.log(domains);
+                   name = domains;
+               }
+               devices.push({
+                   "id": dev.mac[0] * 100 + dev.mac[1],
+                   "name": name,
+                   "mac": dev.mac.toString(),
+                   "host": dev.host.address
+               });
+           });
            node.log(JSON.stringify(dev.host) + " " + JSON.stringify(dev.mac));
 
            dev.on("json", (data) => {
+// JSON.stringify(dev.mac)
                var msg = {payload: data };
 	       node.send(msg);
            });
@@ -39,4 +60,16 @@ module.exports = function(RED) {
         });
     }
     RED.nodes.registerType("broadlink_in", Broadlinkjs);
+
+    RED.httpAdmin.get('/broadlinkjs/devices', function(req, res) {
+        res.json(devices);
+    });
+
+    function BroadlinkConfig(n) {
+        RED.nodes.createNode(this,n);
+        this.device = n.device;
+
+        var node = this;
+    };
+    RED.nodes.registerType('broadlink-dev', BroadlinkConfig);
 }
